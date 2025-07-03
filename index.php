745f79df8cbf60cd07efec9e33df5ed045289ae6
saaycon <craciun_rrazvann@yahoo.com>
2025-07-04 00:54:22 +0100

    html boilerplate and css file for the calendar web app

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar Project</title>
+
+    <meta name="description" content="My Own Calendar Project">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
