d64c1d27ace2ac36c0040bff3559f88144798a6a
saaycon <craciun_rrazvann@yahoo.com>
2025-07-04 01:02:38 +0100

    defining the html structure of the page

--- index.php
+++ index.php
@@ -10,6 +10,24 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+    <header>
+        <h1>📅 Course Calendar<br> My Calendar Project</h1>
+    </header>
+
+    <!-- Clock -->
+    <div class="clock-container">
+        <div id="clock"></div>
+    </div>
+
+    <!-- Calendar Section -->
+     <div class="calendar">
+        <div class="nav-btn-container">
+            <button class="nav-btn">⏮️<button>
+            <h2 id="monthYear" style="margin: 0"></h2>
+            <button class="nav-btn">⏭️</button>
+        </div>
+     </div>
+
 </body>
 </html>
